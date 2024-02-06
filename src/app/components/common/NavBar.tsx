c0171fccb0d08e0dc231307c91c5ce48cddba6be
Lukyord <98599164+Lukyord@users.noreply.github.com>
2024-02-06 20:01:08 +0700

    📦 init shadcn, 🍕 style navbar

--- src/app/components/common/NavBar.tsx
+++ src/app/components/common/NavBar.tsx
@@ -1,15 +1,21 @@
-import Link from "next/link";
+"use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 import { GiMushroomGills } from "react-icons/gi";
 
 export default function NavBar() {
+  const currentPath = usePathname();
+
   const links = [
     { href: "/", label: "Dashboard" },
     { href: "/issues", label: "Issues" },
   ];
 
   return (
-    <nav className="flex gap-8 border-b mb-5 px-5 h-14 items-center">
+    <nav className="flex border-b mb-5 px-5 h-14 items-center gap-8">
       <Link href="/">
         <GiMushroomGills size={30} />
       </Link>
@@ -18,7 +24,13 @@ export default function NavBar() {
           <li key={index}>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={`
+                    ${cn({
+                      "text-zinc-800": currentPath === link.href,
+                      "text-zinc-500": currentPath !== link.href,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                  `}
             >
               {link.label}
             </Link>
