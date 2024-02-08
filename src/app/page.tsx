710bec446cade8e54c3aa450a5a1f94887049dd6
Lukyord <98599164+Lukyord@users.noreply.github.com>
2024-02-08 14:28:12 +0700

    📦 setup clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
 import { Button } from "@/components/ui/button"
 import prisma from "@prisma_/client"
 
-async function getTempAndHumidity() {
-  const records = await prisma.temperatureHumidity.findMany({})
+// async function getTempAndHumidity() {
+//   const records = await prisma.temperatureHumidity.findMany({})
 
-  return records
-}
+//   return records
+// }
 
 export default async function Home() {
-  const records = await getTempAndHumidity()
+  // const records = await getTempAndHumidity()
 
-  return (
-    <div>
-      <Button className="m-4">Check Temp & Humidity</Button>
-      <ul className="px-8 py-4">
-        {records.map((record) => (
-          <li key={record.id}>
-            <p>Date: {record.date}</p>
-            <p>Time: {record.hourOfDay}:00</p>
-            <p>Temperature: {record.temperature}</p>
-            <p>Humidity: {record.humidity}</p>
-            <br />
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+  return <div></div>
 }
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { UserButton } from "@clerk/nextjs"
 
 import { cn } from "@/lib/utils"
 import { GiMushroomGills } from "react-icons/gi"
@@ -15,28 +16,32 @@ export default function NavBar() {
   ]
 
   return (
-    <nav className="mb-5 flex h-14 items-center gap-8 border-b px-5">
-      <Link href="/">
-        <GiMushroomGills size={30} />
-      </Link>
-      <ul className="flex gap-8">
-        {links.map((link, index) => (
-          <li key={index}>
-            <Link
-              href={link.href}
-              className={`
+    <nav className="mb-5 flex h-14 items-center justify-between border-b px-5">
+      <div className="flex gap-8">
+        <Link href="/">
+          <GiMushroomGills size={30} />
+        </Link>
+        <ul className="flex gap-8">
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link
+                href={link.href}
+                className={`
                     ${cn({
                       "text-zinc-800": currentPath === link.href,
                       "text-zinc-500": currentPath !== link.href,
                       "transition-colors hover:text-zinc-800": true,
                     })}
                   `}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <UserButton afterSignOutUrl="/" />
     </nav>
   )
 }
